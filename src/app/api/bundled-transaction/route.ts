a17376f3fe3b05bd16f6d5081354cadb33681951
KetanKudikyal <krotusppp@gmail.com>
2024-09-05 14:10:26 +0530

    fix: 🐛 addresses

--- src/app/api/bundled-transaction/route.ts
+++ src/app/api/bundled-transaction/route.ts
@@ -3,13 +3,38 @@ import { USDC_ABI } from "@/abi/USDC_ABI";
 import { BETTING_CONTRACT_ADDRESS, USDC_ADDRESS } from "@/config/addresses";
 import { getXmtpFrameMessage } from "@coinbase/onchainkit/xmtp";
 import { NextRequest, NextResponse } from "next/server";
-import { encodeFunctionData, parseUnits } from "viem";
+import { Address, encodeFunctionData, parseUnits } from "viem";
+import { morphHolesky, spicy } from "viem/chains";
+
+type AddressMap = {
+  [chainId: number]: {
+    USDC_TOKEN_ADDRESS: Address;
+    BETTING_CONTRACT_ADDRESS: Address;
+  };
+};
+
+export const ADDRESS_MAP: AddressMap = {
+  [morphHolesky.id]: {
+    USDC_TOKEN_ADDRESS: "0x94c17DD37ED3Ca85764b35BfD4d1CCc543b1bE3E",
+    BETTING_CONTRACT_ADDRESS: "0x14097485976CB545d743452f66604bEAC141Cc98",
+  },
+  [spicy.id]: {
+    USDC_TOKEN_ADDRESS: "0xF99b791257ab50be7F235BC825E7d4B83942cf38",
+    BETTING_CONTRACT_ADDRESS: "0x9d24c52916A14afc31D86B5Aa046b252383ee444",
+  },
+};
+
+export const getAddressesForChain = (chainId: number) => ADDRESS_MAP[chainId];
 
 async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
   const body = await req.json();
   const state = body.untrustedData.state;
 
   const chain = state.chain;
+
+  const addresses = getAddressesForChain(chain.id);
+
+  console.log("addresses", addresses);
 
   delete body.untrustedData.state;
 
